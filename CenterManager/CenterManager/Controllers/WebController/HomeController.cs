bf61b910fc765f65456f5f742cc268e343098f1f
Lê Thanh Bình <51404183+sieuquayskt3@users.noreply.github.com>
2021-12-02 15:19:51 +0700

    Add subject mvc
    Giao diện trang chủ
    Add: WebSubjectDAO cho web mvc
    Add: SubjectsController cho web mvc
    Xem + Thêm môn học

--- CenterManager/CenterManager/Controllers/WebController/HomeController.cs
+++ CenterManager/CenterManager/Controllers/WebController/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CenterManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,14 @@ namespace CenterManager.Controllers
 {
     public class HomeController : Controller
     {
+
+        // demo
+        WebSubjectDAO dao = new WebSubjectDAO();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
-            return View();
+            return View(dao.GetAllSubjects());
         }
     }
 }
